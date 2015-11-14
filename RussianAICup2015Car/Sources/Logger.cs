09de76268c81982daa619db548ec5fc099b2a29e
Alexander <ivlev.stef@gmail.com>
2015-11-14 23:05:27 +0600

    fast Start code + Tutorial nitro started.

---
+++ RussianAICup2015Car/Sources/Logger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RussianAICup2015Car.Sources {
+    class Logger {
+        void Error(String message, params object[] args) {
+            System.Console.WriteLine("Error: " + message, args);
+        }
+        void Warning(String message, params object[] args) {
+            System.Console.WriteLine("Warning: " + message, args);
+        }
+        void Debug(String message, params object[] args) {
+            System.Console.WriteLine("Debug: " + message, args);
+        }
+        void Info(String message, params object[] args) {
+            System.Console.WriteLine("Info: " + message, args);
+        }
+    }
+}
--- RussianAICup2015Car/Sources/MyStrategy.cs
+++ RussianAICup2015Car/Sources/MyStrategy.cs
@@ -1,16 +1,62 @@
 using System;
 using Com.CodeGame.CodeRacing2015.DevKit.CSharpCgdk.Model;
+using RussianAICup2015Car.Sources;
 
 namespace Com.CodeGame.CodeRacing2015.DevKit.CSharpCgdk {
     public sealed class MyStrategy : IStrategy {
-        public void Move(Car self, World world, Game game, Move move) {
-            move.EnginePower = 1.0D;
-            move.IsThrowProjectile = true;
-            move.IsSpillOil = true;
+        private Logger log = new Logger();
 
-            if (world.Tick > game.InitialFreezeDurationTicks) {
-                move.IsUseNitro = true;
+        public MyStrategy() {
+
+        }
+
+        public void Move(Car self, World world, Game game, Move move) {
+
+            if (world.Tick < game.InitialFreezeDurationTicks) {
+                return;
             }
+
+            double nextWaypointX = (self.NextWaypointX + 0.5D) * game.TrackTileSize;
+            double nextWaypointY = (self.NextWaypointY + 0.5D) * game.TrackTileSize;
+
+
+            double cornerTileOffset = 0.25D * game.TrackTileSize;
+
+            switch (world.TilesXY[self.NextWaypointX][self.NextWaypointY]) {
+                case TileType.LeftTopCorner:
+                    nextWaypointX += cornerTileOffset;
+                    nextWaypointY += cornerTileOffset;
+                    break;
+                case TileType.RightTopCorner:
+                    nextWaypointX -= cornerTileOffset;
+                    nextWaypointY += cornerTileOffset;
+                    break;
+                case TileType.LeftBottomCorner:
+                    nextWaypointX += cornerTileOffset;
+                    nextWaypointY -= cornerTileOffset;
+                    break;
+                case TileType.RightBottomCorner:
+                    nextWaypointX -= cornerTileOffset;
+                    nextWaypointY -= cornerTileOffset;
+                    break;
+            }
+
+            double angleToWaypoint = self.GetAngleTo(nextWaypointX, nextWaypointY);
+            double speedModule = hypot(self.SpeedX, self.SpeedY);
+
+            move.WheelTurn = (angleToWaypoint * 32.0D / Math.PI);
+            move.EnginePower = 0.75D;
+
+            if (speedModule * speedModule * Math.Abs(angleToWaypoint) > 2.5D * 2.5D * Math.PI) {
+                move.IsBrake = true;
+            }
+
+            move.IsUseNitro = true;
+        }
+        
+        
+        public static double hypot(double a, double b) {
+            return Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
         }
     }
 }
