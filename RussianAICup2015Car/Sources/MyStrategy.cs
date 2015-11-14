0a643bdf331112f37b024bb31317ebe8f455500e
Alexander <ivlev.stef@gmail.com>
2015-11-15 03:02:58 +0600

    first my version. without bonus, without weapon, without entity.

--- RussianAICup2015Car/Sources/MyStrategy.cs
+++ RussianAICup2015Car/Sources/MyStrategy.cs
@@ -5,99 +5,262 @@ using System.Collections.Generic;
 
 namespace Com.CodeGame.CodeRacing2015.DevKit.CSharpCgdk {
   public sealed class MyStrategy : IStrategy {
-    private Logger Log = new Logger();
-    private Car Car = null;
-    private World World = null;
-    private Game Game = null;
+    private Logger log = new Logger();
+    private Car self = null;
+    private World world = null;
+    private Game game = null;
+    private Point<int>[] path = null;
+
+    private static Point<int> DirLeft = new Point<int>(-1, 0);
+    private static Point<int> DirRight = new Point<int>(1, 0);
+    private static Point<int> DirUp = new Point<int>(0, -1);
+    private static Point<int> DirDown = new Point<int>(0, 1);
 
     public MyStrategy() {
     }
 
     public void Move(Car self, World world, Game game, Move move) {
-      Car = self;
-      World = world;
-      Game = game;
+      this.self = self;
+      this.world = world;
+      this.game = game;
+
+      if (null == path) {
+        calculatePath();
+      }
 
       if (world.Tick < game.InitialFreezeDurationTicks) {
           return;
       }
 
-      Point<int>[] wayPoints = wayPointsFromCurrent(3);
-      Log.Assert(3 == wayPoints.Length);
+      Point<int> currentWay = new Point<int>((int)(self.X / game.TrackTileSize), (int)(self.Y /game.TrackTileSize));
+      Point<int>[] wayPoints = wayPointsFrom(currentWay, 3);
+      log.Assert(3 == wayPoints.Length);
 
-      Point<double> nextWayPos = convert(wayPoints[1]);
 
-      double cornerTileOffset = 0.25D * game.TrackTileSize;
+      Point<int> posTypeSelfToNext = positionTypeFor(wayPoints[0], wayPoints[1]);
+      Point<int> posTypeNextToNextNext = positionTypeFor(wayPoints[1], wayPoints[2]);
 
-      switch (world.TilesXY[self.NextWaypointX][self.NextWaypointY]) {
-      case TileType.LeftTopCorner:
-        nextWayPos.X += cornerTileOffset;
-        nextWayPos.Y += cornerTileOffset;
-        break;
-      case TileType.RightTopCorner:
-        nextWayPos.X -= cornerTileOffset;
-        nextWayPos.Y += cornerTileOffset;
-        break;
-      case TileType.LeftBottomCorner:
-        nextWayPos.X += cornerTileOffset;
-        nextWayPos.Y -= cornerTileOffset;
-        break;
-      case TileType.RightBottomCorner:
-        nextWayPos.X -= cornerTileOffset;
-        nextWayPos.Y -= cornerTileOffset;
-        break;
+      Point<double> idealPoint = null;
+      double procent = procentToWay(wayPoints[1], wayPoints[0]);
+      if (procent > 0.5) {
+        idealPoint = convert(wayPoints[0]);
+        idealPoint.X += posTypeSelfToNext.X * game.TrackTileSize;
+        idealPoint.Y += posTypeSelfToNext.Y * game.TrackTileSize;
+        idealPoint.X += posTypeNextToNextNext.X * game.TrackTileSize * 0.5;
+        idealPoint.Y += posTypeNextToNextNext.Y * game.TrackTileSize * 0.5;
+      } else {
+        idealPoint = convert(wayPoints[1]);
+        idealPoint.X += posTypeNextToNextNext.X * game.TrackTileSize * 0.5;
+        idealPoint.Y += posTypeNextToNextNext.Y * game.TrackTileSize * 0.5;
       }
 
-      double angleToWaypoint = self.GetAngleTo(nextWayPos.X, nextWayPos.Y);
+      double angleToWaypoint = self.GetAngleTo(idealPoint.X, idealPoint.Y);
       double speedModule = hypot(self.SpeedX, self.SpeedY);
 
-      move.WheelTurn = (angleToWaypoint * 32.0D / Math.PI);
-      move.EnginePower = 1.0D;
+      log.Debug("Speed:{0}", speedModule);
 
-      if (speedModule * speedModule * Math.Abs(angleToWaypoint) > 3.0D * 3.0D * Math.PI) {
-         move.IsBrake = true;
+      if (posTypeSelfToNext.X != posTypeNextToNextNext.X || posTypeSelfToNext.Y != posTypeNextToNextNext.Y) {
+        move.WheelTurn = (angleToWaypoint * 90.0D / Math.PI);
+
+        if (speedModule > 20) {
+          move.IsBrake = true;
+        } else {
+          move.EnginePower = 1.0D;
+        }
+      } else {
+        move.WheelTurn = (angleToWaypoint * 15.0D / Math.PI);
+        move.EnginePower = 1.0D;
       }
+
 
       move.IsUseNitro = true;
     }
 
     private Point<double> convert(Point<int> point) {
-      Log.Assert(null != Game);
+      log.Assert(null != game);
 
-      double nextWaypointX = (point.X + 0.5D) * Game.TrackTileSize;
-      double nextWaypointY = (point.Y + 0.5D) * Game.TrackTileSize;
+      double nextWaypointX = (point.X + 0.5D) * game.TrackTileSize;
+      double nextWaypointY = (point.Y + 0.5D) * game.TrackTileSize;
       return new Point<double>(nextWaypointX, nextWaypointY);
     }
 
-    private Point<int>[] wayPointsFromCurrent(int count) {
-      Log.Assert(null != World);
-      Log.Assert(null != Car);
+    private int wayIndex(Point<int> way) {
+      log.Assert(null != path);
 
-      int currentIndex = 0;
-      for (currentIndex = 0; currentIndex < World.Waypoints.Length; currentIndex++) {
-        int[] point = World.Waypoints[currentIndex];
-        Log.Assert(2 == point.Length);
-
-        if (point[0] == Car.NextWaypointX && point[1] == Car.NextWaypointY) {
-          currentIndex = (currentIndex + World.Waypoints.Length - 1) % World.Waypoints.Length;
-          break;
+      for (int index = 0; index < path.Length; index++) {
+        if (path[index].X == way.X &&  path[index].Y == way.Y) {
+          return index;
         }
       }
 
-      Log.Assert(currentIndex >= 0);
+      log.Assert(false);
+      return 0;
+    }
+
+    private Point<int>[] wayPointsFrom(Point<int> way, int count) {
+      log.Assert(null != way);
+      log.Assert(null != path);
+
+      int currentIndex = wayIndex(way);
+      log.Assert(currentIndex >= 0);
 
       List<Point<int>> points = new List<Point<int>>();
       while (count > 0) {
-        int[] point = World.Waypoints[currentIndex];
-        Log.Assert(2 == point.Length);
+        points.Add(path[currentIndex]);
 
-        points.Add(new Point<int>(point[0], point[1]));
-
-        currentIndex = (currentIndex + 1) % World.Waypoints.Length;
+        currentIndex = (currentIndex + 1) % path.Length;
         --count;
       }
 
       return points.ToArray();
+    }
+
+    private double pixelsToWay(Point<int> way, Point<int> prevWay) {
+      log.Assert(null != way);
+      log.Assert(null != prevWay);
+      log.Assert(null != game);
+
+      double A = way.X - prevWay.X;
+      double B = way.Y - prevWay.Y;
+
+      Point<double> prevWayPixels = convert(prevWay);
+      double Xcenter = prevWayPixels.X + A * game.TrackTileSize * 0.5;
+      double Ycenter = prevWayPixels.Y + B * game.TrackTileSize * 0.5;
+
+      double C = A * Xcenter + B * Ycenter;
+
+      return Math.Abs(A * self.X + B * self.Y - C)/Math.Sqrt(A*A + B*B);
+    }
+
+    private double procentToWay(Point<int> way, Point<int> prevWay) {
+      log.Assert(null != way);
+      log.Assert(null != prevWay);
+      log.Assert(null != game);
+
+      return pixelsToWay(way, prevWay) / game.TrackTileSize;
+    }
+
+    private Point<int> positionTypeFor(Point<int> way, Point<int> nextWay) {
+      Point<int> posType = new Point<int>(nextWay.X - way.X, nextWay.Y - way.Y);
+      
+      log.Assert(posType.Equals(DirLeft) ||
+                 posType.Equals(DirRight) ||
+                 posType.Equals(DirUp) ||
+                 posType.Equals(DirDown));
+
+      return posType;
+    }
+
+    private Point<int> startDirection() {
+      switch (world.StartingDirection) {
+      case Direction.Left:
+        return DirLeft;
+      case Direction.Right:
+        return DirRight;
+      case Direction.Up:
+        return DirUp;
+      case Direction.Down:
+        return DirDown;
+      }
+      return new Point<int>(0);
+    }
+
+    private void calculatePath() {
+      log.Assert(null != world);
+      log.Assert(world.Waypoints.Length >= 2);
+
+      List<Point<int>> newPath = new List<Point<int>>();
+
+      Point<int> direction = startDirection();
+
+      for (int index = 0; index < world.Waypoints.Length; index++) {
+        Point<int> begin = new Point<int>(world.Waypoints[index][0], world.Waypoints[index][1]);
+        int nextIndex = (index+1) % world.Waypoints.Length;
+        Point<int> end = new Point<int>(world.Waypoints[nextIndex][0], world.Waypoints[nextIndex][1]);
+
+        Tuple<List<Point<int>>, Point<int>> subpath = calculatePath(begin, end, direction);
+        log.Assert(null != subpath);
+
+        newPath.AddRange(subpath.Item1);
+        direction = subpath.Item2;
+      }
+
+
+      path = newPath.ToArray();
+    }
+
+    private Tuple<List<Point<int>>, Point<int>> addIfNeed(Tuple<List<Point<int>>, Point<int>> data, Point<int> point) {
+      if (null == data) {
+        return null;
+      }
+      data.Item1.Insert(0, point);
+      return data;
+    }
+
+    private Tuple<List<Point<int>>, Point<int>> calculatePath(Point<int> current, Point<int> end, Point<int> direction) {
+      log.Assert(null != world);
+
+      if (current.X == end.X && current.Y == end.Y) {
+        return new Tuple<List<Point<int>>,Point<int>>(new List<Point<int>>(), new Point<int>(direction));
+      }
+
+      log.Assert(0 <= current.X && current.X < world.Width);
+      log.Assert(0 <= current.Y && current.Y < world.Height);
+
+      switch (world.TilesXY[current.X][current.Y]) {
+      case TileType.Empty:
+        log.Assert(false);
+        return null;
+      case TileType.Vertical:
+        if (direction.Y > 0) {
+          return addIfNeed(calculatePath(current.Add(DirDown), end, DirDown), current);
+        } else {
+          return addIfNeed(calculatePath(current.Add(DirUp), end, DirUp), current);
+        }
+      case TileType.Horizontal:
+        if (direction.X > 0) {
+          return addIfNeed(calculatePath(current.Add(DirRight), end, DirRight), current);
+        } else {
+          return addIfNeed(calculatePath(current.Add(DirLeft), end, DirLeft), current);
+        }
+      case TileType.LeftTopCorner:
+        if (direction.X < 0) {
+          return addIfNeed(calculatePath(current.Add(DirDown), end, DirDown), current);
+        } else {
+          return addIfNeed(calculatePath(current.Add(DirRight), end, DirRight), current);
+        }
+      case TileType.RightTopCorner:
+        if (direction.X > 0) {
+          return addIfNeed(calculatePath(current.Add(DirDown), end, DirDown), current);
+        } else {
+          return addIfNeed(calculatePath(current.Add(DirLeft), end, DirLeft), current);
+        }
+      case TileType.LeftBottomCorner:
+        if (direction.X < 0) {
+          return addIfNeed(calculatePath(current.Add(DirUp), end, DirUp), current);
+        } else {
+          return addIfNeed(calculatePath(current.Add(DirRight), end, DirRight), current);
+        }
+      case TileType.RightBottomCorner:
+        if (direction.X > 0) {
+          return addIfNeed(calculatePath(current.Add(DirUp), end, DirUp), current);
+        } else {
+          return addIfNeed(calculatePath(current.Add(DirLeft), end, DirLeft), current);
+        }
+      case TileType.LeftHeadedT:
+        break;
+      case TileType.RightHeadedT:
+        break;
+      case TileType.TopHeadedT:
+        break;
+      case TileType.BottomHeadedT:
+        break;
+      case TileType.Crossroads:
+        break;
+      }
+
+      log.Assert(false);
+      return null;
     }
         
     private static double hypot(double a, double b) {
--- RussianAICup2015Car/Sources/Point.cs
+++ RussianAICup2015Car/Sources/Point.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace RussianAICup2015Car.Sources {
-  class Point<Type> {
+  class Point<Type> where Type : struct {
     public Type X;
     public Type Y;
 
@@ -17,6 +17,23 @@ namespace RussianAICup2015Car.Sources {
     public Point(Type x, Type y) {
       X = x;
       Y = y;
+    }
+
+    public Point(Point<Type> p) {
+      X = p.X;
+      Y = p.Y;
+    }
+
+    public bool Equals(Point<Type> p) {
+      if ((object)p == null) {
+        return false;
+      }
+
+      return ((dynamic)X == (dynamic)p.X) && ((dynamic)Y == (dynamic)p.Y);
+    }
+
+    public Point<Type> Add(Point<Type> p) {
+      return new Point<Type>((dynamic)X + (dynamic)p.X, (dynamic)Y + (dynamic)p.Y);
     }
   }
 }
